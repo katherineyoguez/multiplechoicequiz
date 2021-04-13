9997397b24bb9c91e4f448514889057c3ebfea79
Katherine Yoguez <katherinecarlotayoguez@gmail.com>
2021-04-12 23:13:13 -0400

    added to html and css

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+var questions = [{
+    "question": "What does CSS stand for ?",
+    "option1": "Colorful Style Sheets",
+    "option2": "Cascading Style Sheets",
+    "option3": "Creative Style Sheets",
+    "option4": "Computer Style Sheets",
+    "answer": "2"
+}, {
+    "question": "What is the correct HTML for referring to an external style sheet ?",
+    "option1": "<link rel="stylesheet" type="text/css" href="mystyle.css">",
+    "option2": "<stylesheet>mystylesheet.css</stylesheet>",
+    "option3": "<style src="mystyle.css">",
+    "option4": "<div> stylesheet </div>",
+    "answer": "1"
+}, {
+    "question": "How many typles of heading tags does HTML contain ?",
+    "option1": "4",
+    "option2": "5",
+    "option3": "2",
+    "option4": "6",
+    "answer": "4"
+
+}, {
+    "question": "Where is the correct place to insert a JavaScript?",
+    "option1": "The <footer> section.",
+    "option2": "The <head> section.",
+    "option3": "both the <head> and <body> section.",
+    "option4": "The <body> section",
+    "answer": "4"
+}, {
+    "question": "How do you add a comment in JavaScript?",
+    "option1": "*//",
+    "option2": "!--",
+    "option3": "//",
+    "option4": "//*",
+    "answer": "3"
+},
