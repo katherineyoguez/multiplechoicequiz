6770bd2e99007b1d408885c767716ba7ec8aebb9
Katherine Yoguez <katherinecarlotayoguez@gmail.com>
2021-04-17 22:03:42 -0400

    updated css html and javascript

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+var currentQuestion = 0;
+var score = 0;
+// var totQuestions = questions.length;
 var questions = [{
     "question": "What does CSS stand for ?",
     "option1": "Colorful Style Sheets",
@@ -6,14 +9,14 @@ var questions = [{
     "option4": "Computer Style Sheets",
     "answer": "2"
 }, {
-    "question": "What is the correct HTML for referring to an external style sheet ?",
-    "option1": "<link rel="stylesheet" type="text/css" href="mystyle.css">",
-    "option2": "<stylesheet>mystylesheet.css</stylesheet>",
-    "option3": "<style src="mystyle.css">",
-    "option4": "<div> stylesheet </div>",
-    "answer": "1"
-}, {
-    "question": "How many typles of heading tags does HTML contain ?",
+    //     "question": "What is the correct HTML for referring to an external style sheet ?",
+    //     "option1": "<link rel="stylesheet" type="text / css" href="mystyle.css">",
+    //     "option2": "<stylesheet>mystylesheet.css</stylesheet>",
+    //     "option3": "<style src="mystyle.css">",
+    //     "option4": "<div> stylesheet </div>",
+    //     "answer": "1"
+    // }, {
+    "question": "How many types of heading tags does HTML contain ?",
     "option1": "4",
     "option2": "5",
     "option3": "2",
@@ -34,4 +37,54 @@ var questions = [{
     "option3": "//",
     "option4": "//*",
     "answer": "3"
-},
+}]
+var totQuestions = questions.length.opt1, opt2, opt3, opt4;
+var totalQuestions = questions.length;
+
+var container = document.getElementById('quizContainer');
+var questionEl = document.getElementById('question');
+var opt1 = document.getElementById('opt1');
+var opt2 = document.getElementById('opt2');
+var opt3 = document.getElementById('opt3');
+var opt4 = document.getElementById('opt4');
+var nextButton = document.getElementById('nextButton');
+var resultCount = document.getElementById('result');
+
+function loadQuestion(questionIndex) {
+    var q = questions[questionIndex];
+    questionEl.textContent = (questionIndex + 1) + '. ' + q.question;
+    opt1.textContent = q.option1;
+    opt2.textContent = q.option2;
+    opt3.textContent = q.option3;
+    opt4.textContent = q.option4;
+};
+
+function loadNextQuestion() {
+    var selectedOption = document.querySelector('input[type=radio]:checked');
+    if (!selectedOption) {
+        alert('Please select your answer');
+        return;
+    }
+    var answer = selectedOption.value;
+    if (questions[currentQuestion].answer == answer) {
+        score += 10;
+    }
+    selectedOption.checked = false;
+    currentQuestion++;
+
+    if (currentQuestion == totalQuestions -1 ) {
+        nextButton.textContent = 'Finish';
+
+    }
+    if(currentQuestion == totalQuestions);
+    container.style.display = 'none';
+    resultCount.style.display = '';
+    resultCount.textContent = 'Your Score: ' + score;
+    return;
+
+}
+
+loadQuestion(currentQuestion);
+// }
+
+// loadQuestion(currentQuestion);
